007bf488162e1c07bf21ccf8e801516616caf7dd
ViniciusM11 <noturno793@gmail.com>
2021-08-30 18:51:28 -0300

    Atividade 1 - DS

---
+++ DS/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade01
+{
+    // Classe Conta
+    class Conta
+    {
+        // Atributos
+        private int Agencia { get; set; }
+        private string ContaCorrente { get; set; }
+
+
+        // Métodos
+        public void Depositar(float valor)
+        {
+
+        }
+
+        public float Saldo(int saldo)
+        {
+            return saldo;
+        }
+    }
+
+    // Classe ContaPoupanca
+    class ContaPoupanca
+    {
+        // Atributos
+        private int DiaDeposito { get; set; }
+
+        // Métodos
+        public float VerLucro()
+        {
+            return 0;
+        }
+
+    }
+
+    // Classe Cliente
+    class Cliente
+    {
+        // Atributos
+        private int CPF { get; set; }
+        private string Nome { get; set; }
+        private string Telefone { get; set; }
+
+        // Métodos
+        public int MostrarCPF()
+        {
+            return this.CPF;
+        }
+
+        public void VerSaldo(int saldo)
+        {
+
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+            ContaPoupanca contaPoupanca = new ContaPoupanca();
+            Cliente cliente = new Cliente();
+        }
+    }
+}
