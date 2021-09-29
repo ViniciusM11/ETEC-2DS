c79420c7506d74d8c7894afe38684d6f3c6b1b7c
ViniciusM11 <noturno793@gmail.com>
2021-09-28 22:18:40 -0300

    Exemplo Veiculo

---
+++ DS/Veiculo/Veiculo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Veiculo
+{
+    // Classe Veiculo
+    class Veiculo
+    {
+        //atributo
+        public string Placa { get; set; }
+
+        //método
+        public bool DefinirPlaca(string placa)
+        {
+            Regex rgx = new Regex("^[A-Z]{3}[0-9]{4}||[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}$"); // valida a placa, se estiver tudo conforme, retorna verdadeiro
+
+            if (rgx.Match(placa).Success)
+            {
+                Placa = placa;
+                return true;
+            }
+            return false;
+
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo veiculo = new Veiculo();
+
+            if (veiculo.DefinirPlaca("RMX7C25"))
+            {
+                Console.WriteLine($"Aluno {veiculo.Placa} cadastrado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Erro ao cadastrar placa!");
+            }
+        }
+    }
+}
