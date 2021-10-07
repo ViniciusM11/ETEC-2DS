a20c963742d24eb0605f597193d75c0d00fb5e01
ViniciusM11 <noturno793@gmail.com>
2021-10-06 22:38:23 -0300

    Adicionar Imgs

--- PAM1/Navegacao/App.js
+++ PAM1/Navegacao/App.js
@@ -1,24 +1,34 @@
 import * as React from "react";
-import { Button, View, Text } from "react-native";
+import { Image, StyleSheet, TouchableOpacity, Button, View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LImg from "./assets/LImg.jpg";
+import KImg from "./assets/KiraImg.jpg";
+
+
 
 const JanelaInicial = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{paddingBottom: 50}}>Qual vc prefere?</Text>
-      <Button title="L" onPress={()=> navigation.navigate ("L")}></Button>
-      <Button title="KIRA" onPress={()=> navigation.navigate ("Kira")}></Button>
+    <View style={styles.container}>
+      <Text>Qual você prefere?</Text>
+      <TouchableOpacity style={styles.botao} onPress={() => navigation.navigate("Lawliet")}>
+        <Text style={styles.botao1}>L</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.botao} onPress={() => navigation.navigate("Kira")}>
+        <Text style={styles.botao1}>KIRA</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const Stack = createNativeStackNavigator();
 
-const L = () => {
+const Lawliet = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Boa Escolha</Text>
+      <Text style={{fontSize: 30}} >Boa Escolha</Text>
+      <Image style={styles.img} source={LImg}></Image>
     </View>
   );
 };
@@ -26,7 +36,8 @@ const L = () => {
 const Kira = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Boa Escolha</Text>
+      <Text style={{fontSize: 30}} >Boa Escolha</Text>
+      <Image style={styles.img} source={KImg}></Image>
     </View>
   );
 };
@@ -39,17 +50,44 @@ const App = () => {
         screenOptions={{ headerTitle: "?" }}
       >
         <Stack.Screen name="Inicial" component={JanelaInicial} />
-        <Stack.Screen
-          name="Kira"
-          component={Kira}
-        />
-        <Stack.Screen
-          name="L"
-          component={L}
-        />
+        <Stack.Screen name="Kira" component={Kira} />
+        <Stack.Screen name="Lawliet" component={Lawliet} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    alignItems: "center", 
+    justifyContent: "center",
+    padding: 55
+  },
+
+  botao: {
+    padding: 5,
+    width: 100,
+    height: 50
+  },
+
+  botao1: {
+    width: 100,
+    height: 30,
+    backgroundColor: '#7883FF',
+    textAlign: "center"
+    
+  },
+
+  img: {
+    padding: 8,
+    margin:48, 
+    width: 400, 
+    height:400
+  }
+
+});
 
 export default App;
